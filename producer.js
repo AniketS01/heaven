4761c95b3bf0ef44cdf77c00abc0b36ac21acbee
AniketS01 <aniketshetyeajs@gmail.com>
2024-11-20 13:57:24 +0530

    updated

--- producer.js
+++ producer.js
@@ -1,5 +1,5 @@
 const io = require("socket.io-client");
-const socket = io("http://172.20.10.5:3000");
+const socket = io("http://172.20.10.5");
 const figlet = require('figlet')
 
 figlet("Heaven", function (err, data) {
